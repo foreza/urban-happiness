5250195aff41db9a12a5476c3ed37d7e7cdc8214
Jason Chiu <jforeza@gmail.com>
2021-06-18 11:54:55 -0700

    Clean comments, add read me, do csv output

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # urban-happiness
+
+This node script parses data for TripleLift.
+
+
+## Prerequisites: 
+- Node.js v14.11.0 (or later)
+- A working internet connection (hah!)
+
+
+## Installation
+
+### Step 1:
+
+Clone the repository.
+
+```
+git clone https://github.com/foreza/urban-happiness.git
+```
+
+### Step 2: 
+
+Run `npm i` to install all dependencies.
+
+You're all set. 
+
+
+## Usage
+
+`parser.js` will take a CSV file as input.
+
+### Test Run on a smaller data set:
+
+```js
+node parser.js sample_data/tactic-small.csv
+```
+
+
+### Actual run with a larger data set
+
+```js
+node parser.js sample_data/tactic.csv`
+```
+
+
+### Output:
+
+You'll get a CSV with any URLs that failed in the data set.
+
+Consult the `sample_data` directory for examples.
--- parser.js
+++ parser.js
@@ -2,43 +2,46 @@ const csv = require('csv-parser');
 const axios = require('axios');
 const fs = require('fs');
 
-const LOGTAG = "~~ [TL TACTIC TOOL]: ";
 
-// CHANGE THIS (TODO: read from args)
-const tacticFileName = "tactic.csv";
-// const tacticFileName = "tactic-small.csv";
+/* Global Vars */
 
+const LOGTAG = "~~ [TL TOOL]: ";      // Logging constant
+const BATCH_SIZE = 20;
+const BATCH_DELAY_MS = 50;
 
-var expectedTotalRequests = 0;
-var totalRequestCount = 0;
-var interval;
+// For reporting requests
+var interval;                         // Reference to the logger setInterval object so we can destroy later
+var expectedTotalRequests = 0;        // Tracks the # of total requests so we don't have to continue asking for the object length
+var totalRequestCount = 0;            // Incremented every time a single request is queued up - consumed by logger
 
-var successCount = 0;
-var failureCount = 0;
-var failedURLMap = [];             // After processing the urls to see which ones are successful
+var successCount = 0;                 // Tracks the # of successful http requests (2xx/3xx)
+var failureCount = 0;                 // Tracks the # of failed requests (4xx,5xx,timeout, invalid url)
+var failedURLMap = [];                // Stores failed urls to be output later at the end of the batch run
 
-
-var tacticData = [];                 // processCSV will provide this if successful.
-var jsImpURLCollection = {};              // After the process CSV step, we'll provide this to our batching function
-
+// Utility for logging messages
 var logger = (msg) => {
   console.log(LOGTAG + msg);
 }
 
-// Read in the CSV file and process it.
+
+// Read in the CSV file as the first argument and process it.
 var processCSV = () => {
 
+  var myArgs = process.argv.slice(2);
+  logger("Processing: " + myArgs[0]);
+
+  var tacticData = [];
+  var jsImpURLCollection = {};
+
   // Read in the tactic data
-  fs.createReadStream(tacticFileName)
+  fs.createReadStream(myArgs[0])
     .pipe(csv())
     .on('data', (row) => {
       tacticData.push(row);
     })
     .on('end', () => {
-      logger('CSV file successfully processed');
-      // console.log(tacticData);
+      logger('CSV file successfully read, checking impression URLs');
       jsImpURLCollection = obtainImpressionURLs(tacticData);
-      // console.log(jsImpURLCollection);
       verifyImpressionURLs(jsImpURLCollection);
     });
 }
@@ -57,7 +60,6 @@ var obtainImpressionURLs = (data) => {
 
     // Verify that the url(s) are valid
     if (isValidURLCollection(urlTargets)) {
-      //console.log("url targets:", urlTargets.length);
 
       // Loop through each valid row
       for (var j = 0; j < urlTargets.length; ++j) {
@@ -79,20 +81,17 @@ var obtainImpressionURLs = (data) => {
 }
 
 
-
 // Verify the tactic has provided valid impression URLs
 var isValidURLCollection = (urlTargets) => {
   if (urlTargets.length == 0 || urlTargets[0] == "[]" || urlTargets == "NULL") {
     return false;
   }
-  // console.log(urlTargets)
   return true;
 }
 
 
 // Grab the urls from each tactic
 var cleanURLTargets = (urlArr) => {
-  // console.log(urlArr);
   var collection = [];
   var splitByQuotes = urlArr.split("\"");
   splitByQuotes.map(function (a) {
@@ -100,7 +99,6 @@ var cleanURLTargets = (urlArr) => {
       collection.push(a);
     }
   });
-  // console.log("Finished: ", collection);
   return collection;
 }
 
@@ -110,7 +108,7 @@ var checkProcessingStatus = () => {
   // Report whenever we can.
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
-  process.stdout.write(`Current Status: [${totalRequestCount}/${expectedTotalRequests}] ${Math.round(100*totalRequestCount/expectedTotalRequests)}%`);
+  process.stdout.write(`Current Status: [${totalRequestCount}/${expectedTotalRequests}] ${Math.round(100 * totalRequestCount / expectedTotalRequests)}%`);
 }
 
 
@@ -121,7 +119,7 @@ var processBatchAfterDelayMS = (collection, delay, startingIndex, batchSize) => 
       var t = collection[i];
       if (t) {
         totalRequestCount++;
-        doRequestForURLAndTactic(totalRequestCount, t.impression_url_arr[0], t.tactic_id);
+        doRequestForURLAndTactic(t.impression_url_arr[0], t.tactic_id);
       } else {
         // TODO: handle this
         // We shouldn't reach here...  
@@ -142,37 +140,31 @@ var processBatchAfterDelayMS = (collection, delay, startingIndex, batchSize) => 
 }
 
 
-
-var doRequestForURLAndTactic = (reqID, url, tactic) => {
-  // logger(`[${reqID}/${expectedTotalRequests}] Tactic: ${tactic} URL: ${url}`);
+// Uses axios to do a request and report on the status
+var doRequestForURLAndTactic = (url, tactic) => {
   try {
-    url = JSON.parse(`"${url}"`);     // Hacky - there's got to be a better way
+    url = JSON.parse(`"${url}"`);     // TODO: Hacky - there's got to be a better way
   } catch (e) {
-
     // If a url fails the JSON parse for whatever reason, log it as a failure and short terminate. (for now)
-
     failureCount++;
-    failedURLMap.push([tactic, url]);
+    failedURLMap.push({ tactic: tactic, url: url });
     return;
   }
-
 
   axios.get(url, { timeout: 1000 }).then(function (response) {
 
     // Check to ensure our response is a 2XX or 3XX
-
     if (response.status.toString()[0] === "2" || response.status.toString()[0] === "3") {
       successCount++;
     }
   }).catch(function (error) {
 
     // If the request failed for whatever reason, add to the failure count. Timeouts, 404, etc
-
     failureCount++;
-    failedURLMap.push([tactic, url]);
+    failedURLMap.push({ tactic: tactic, url: url });
   }).finally(function () {
 
-    // Print the results when we are done.
+    // Only actually report the results when we are done - we shouldn't drop anything.
     if (failureCount + successCount == expectedTotalRequests) {
       printResults();
     }
@@ -183,44 +175,58 @@ var doRequestForURLAndTactic = (reqID, url, tactic) => {
 }
 
 
-
-
 // Starts the batch job.
 var verifyImpressionURLs = (collection) => {
-
   /*
     Batch these in groups of "X" so we're not overloading our poor server / computer...
     Queue up batchSize # of requests and execute them.
     TODO: Tweak these to find a good balance.
   */
   expectedTotalRequests = collection.length;
-  processBatchAfterDelayMS(collection, 40, 0, 20);
+  processBatchAfterDelayMS(collection, BATCH_DELAY_MS, 0, BATCH_SIZE);
   // Set up a logger to periodically report status back
-  interval = setInterval(checkProcessingStatus, 1000); 
-
+  interval = setInterval(checkProcessingStatus, 1000);
 }
 
 
-
+// Print out the results.
 var printResults = () => {
+  logger("CSV BATCH RUN SUMMARY:");
+  console.log("========================")
+  console.log(`\n\nNumber OK (2xx and 3xx Responses):\n${successCount}/${totalRequestCount}
+  \nNumber Failed (4xx and 5xx Responses):\n${failureCount}/${totalRequestCount}\n\n`);
+  // List the Tactic ID & URLs that failed
+  prettyPrintFailureURL(failedURLMap);
+}
 
-  // Success: 3246, Failure: 20618
-  // Success: 3246, Failure: 20618
+
+// Pretty print out the failure URLs so we can consume this elsewhere if we desire
+var prettyPrintFailureURL = (urlArr) => {
+  // console.log("Failed URLs and their tactic in CSV: \n ");
+  // console.log("========================")
+  // console.log("Tactic,URL")
+  // for (var i = 0; i < urlArr.length; ++i) {
+  //   console.log(`${urlArr[0]}, ${urlArr[1]}`)
+  // }
+  // console.log("========================")
 
 
-  //   Success: 18710, Failure: 5154
-  //   Success: 18997, Failure: 4867
-  //  Success: 18965, Failure: 4899
+  let filename = `failed-url-results-${Date.now()}.csv`;
+  const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+  const csvWriter = createCsvWriter({
+    path: filename,
+    header: [
+      { id: 'tactic', title: 'Tactic' },
+      { id: 'url', title: 'URL' },
+    ]
+  });
 
+  csvWriter.writeRecords(urlArr)
+    .then(() => logger(`${filename} has been created. \n There are ${urlArr.length} entries to review.`));
 
-  logger(`Success: ${successCount}, Failure: ${failureCount}`);
-  logger("Failed URLs and their tactic: ", failedURLMap);
 
 }
 
 
-
-
-
-// Invoke the process.
+// Actual entry point.
 processCSV();
